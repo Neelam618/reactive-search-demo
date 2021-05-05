c16b4930451f05f2673a0bb50ec6df83a64d2fec
Neelam618 <neelamchavan410@gmail.com>
2021-05-05 11:13:19 +0530

    added category search

--- src/App.js
+++ src/App.js
@@ -15,18 +15,19 @@ const Main = () => (
 		app="myappdemo"
     credentials='cnjg9Mh5k:c18fa74e-33d4-4460-807c-712b54e002f4' 
 	>
-  <CategorySearch
-    componentId="searchbox"
-    dataField="model"
-    categoryField="brand.keyword"
-    placeholder="Search"
-    style={{
-        padding: '5px',
-        marginTop: '10px',
-    }}
-  />
-		<div className="row reverse-labels">
-			<div className="col">
+	<CategorySearch
+		componentId="searchbox"
+		dataField="model"
+		categoryField="original_title.keyword"
+		placeholder="Search"
+		style={{
+			padding: '5px',
+			marginTop: '10px',
+			marginBottom: '3rem'
+		}}
+	/>
+	<div className="row reverse-labels">
+		<div className="col">
 			<SingleDropdownRange
 					componentId="BookSensor"
 					dataField="average_rating"
@@ -36,59 +37,57 @@ const Main = () => (
 						{ start: 3, end: 4, label: 'Rating 3 to 4' },
 						{ start: 4, end: 5, label: 'Rating > 4' },
 					]}
-				/>
-			</div>
-			<div className="col" style={{ backgroundColor: '#fafafa' }}>
-				<ReactiveList
-					componentId="SearchResult"
-					dataField="original_title"
-					size={10}
-					pagination
-					URLParams
-					react={{
-						and: 'BookSensor',
-					}}
-					render={({ data }) => (
-						<ReactiveList.ResultCardsWrapper>
-							{data.map(item => (
-								<ResultCard key={item.id}>
-									<ResultCard.Image src={item.image} />
-									<ResultCard.Title>
-										<div
-											className="book-title"
-											dangerouslySetInnerHTML={{
-												__html: item.original_title,
-											}}
-										/>
-									</ResultCard.Title>
-
-									<ResultCard.Description>
-										<div className="flex column justify-space-between">
-											<div>
-												<div>
-													by{' '}
-													<span className="authors-list">
-														{item.authors}
-													</span>
-												</div>
-												<div className="ratings-list flex align-center">
-                        <ResultCard.Description>
-                            {'*'.repeat(item.average_rating_rounded)}
-                        </ResultCard.Description>
-												</div>
-											</div>
-											<span className="pub-year">
-												Pub {item.original_publication_year}
-											</span>
-										</div>
-									</ResultCard.Description>
-								</ResultCard>
-							))}
-						</ReactiveList.ResultCardsWrapper>
-					)}
-				/>
-			</div>
+			/>
 		</div>
+		<div className="col" style={{ backgroundColor: '#fafafa' }}>
+			<ReactiveList
+				componentId="SearchResult"
+				dataField="original_title"
+				size={10}
+				pagination
+				URLParams
+				react={{
+					and: 'BookSensor',
+				}}
+				render={({ data }) => (
+					<ReactiveList.ResultCardsWrapper>
+						{data.map(item => (
+							<ResultCard key={item.id}>
+								<ResultCard.Image src={item.image} />
+								<ResultCard.Title>
+									<div
+										className="card-title"
+										dangerouslySetInnerHTML={{
+											__html: item.original_title,
+										}}
+									/>
+								</ResultCard.Title>
+
+								<ResultCard.Description>
+									<div className="flex column justify-space-between">
+										<div>
+											<div>
+												by{' '}
+												<span className="authors-list">
+													{item.authors}
+												</span>
+											</div>
+											<div className="ratings-list">
+												{'*'.repeat(item.average_rating_rounded)}
+											</div>
+										</div>
+										<span className="pub-year">
+											Pub {item.original_publication_year}
+										</span>
+									</div>
+								</ResultCard.Description>
+							</ResultCard>
+						))}
+					</ReactiveList.ResultCardsWrapper>
+				)}
+			/>
+		</div>
+	</div>
 	</ReactiveBase>
 );
 
